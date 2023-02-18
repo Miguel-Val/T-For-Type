05b70c5eb1ba72a4f8c28e3d5aa3642cbef2fc25
sleepyycode <chahpo@gmail.com>
2023-02-17 23:45:37 -0500

    Initialize project using Create React App

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+import FrontPage from './components/FrontPage';
+import NavBar from './components/Navbar';
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+
+
+function App () {
+  
+  return (
+    FrontPage()
+    
+  
+      
+
+
+  );
+  }
+
+
+
+export default App;
---
+++ src/components/FrontPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { Typewriter } from 'react-simple-typewriter';
+
+function FrontPage() {
+  return (
+    <>
+    <div className='Front Page'>
+    <h1 style={{margin: '0', display:'flex', placecontent:'center', textalign:'center', justifyContent:'center', height:'24em', alignItems:'center', top:'50%', left:'50%', transitionDelay:'2s',}}>
+        <span style={{ color: 'black', fontWeight: 'bold', fontSize: '40px',}}>
+          <Typewriter
+            words={['T For ']}
+            typeSpeed={180} 
+          />
+          <span style={{transitionDelay: '22s',}}>
+          <Typewriter
+            words={['     Typ3','  Type']}
+            
+            cursor
+            cursorStyle='|'
+            cursorBlinking={true}
+            typeSpeed={190} 
+          />
+          </span>
+          </span>
+      </h1>
+      </div>
+    </>
+  )
+}
+
+export default FrontPage;
